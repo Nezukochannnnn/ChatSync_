b7f5d8df5c0443a3b471d9d70d1a42b1d912df31
Saksham Jaiswal <saksham2607jaiswal@gmail.com>
2026-07-24 02:38:39 +0530

    Clean up project structure, remove unused module and drawables, and optimize build setup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.google.gms.google.services)
     alias(libs.plugins.kotlin.kapt)
 }
@@ -36,7 +35,6 @@ android {
     buildFeatures {
         dataBinding = true
         viewBinding = true
-        compose = true
     }
 }
 kotlin {
@@ -45,35 +43,24 @@ kotlin {
     }
 }
 
-
 dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.compose.ui)
-    implementation(libs.androidx.compose.ui.graphics)
-    implementation(libs.androidx.compose.ui.tooling.preview)
-    implementation(libs.androidx.compose.material3)
     implementation(platform(libs.firebase.bom))
     implementation(libs.firebase.firestore)
-    implementation(libs.play.services.base)
-    implementation("com.mikhaellopez:circularimageview:4.3.1")
+    implementation(libs.firebase.auth)
+    implementation(libs.firebase.storage)
     implementation("com.google.firebase:firebase-analytics")
+    implementation(libs.play.services.base)
     implementation(libs.google.android.material)
     implementation(libs.androidx.coordinatorlayout)
     implementation(libs.androidx.appcompat)
-    implementation(libs.firebase.auth)
-    implementation(libs.firebase.storage)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation("com.mikhaellopez:circularimageview:4.3.1")
     implementation(libs.picasso)
     implementation("androidx.cardview:cardview:1.0.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
-    debugImplementation(libs.androidx.compose.ui.tooling)
-    debugImplementation(libs.androidx.compose.ui.test.manifest)
 }
